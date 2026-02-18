38de4da5e5f9b5d8c10286978dc9dc2187b2d907
Syamili Deepu <syamilideepu07@gmail.com>
2026-02-18 22:30:29 +0530

    UC2: Added banner printing implementation

--- OOPSBannerAPP.java
+++ OOPSBannerAPP.java
@@ -1,7 +1,13 @@
 public class OOPSBannerAPP {
 
-
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  *****   " + "  *****   " + " ******** " + " ******** ");
+        System.out.println(" *     *  " + " *     *  " + " *      * " + " *        ");
+        System.out.println(" *     *  " + " *     *  " + " *      * " + " *        ");
+        System.out.println(" *     *  " + " *     *  " + " ******** " + " ******** ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + "        * ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + "        * ");
+        System.out.println("  *****   " + "  *****   " + " *        " + " ******** ");
     }
 }
