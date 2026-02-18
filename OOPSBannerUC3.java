6cc2e066254b9a370ccf2a8a56261f0868d3619a
Syamili Deepu <syamilideepu07@gmail.com>
2026-02-18 22:43:18 +0530

    UC3: Refactored banner using String.join()

---
+++ OOPSBannerUC3.java
@@ -0,0 +1,33 @@
+public class OOPSBannerUC3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                " ***** ", " ***** ", " ******", " ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*     *", "*     *"
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*     *", "*"
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "****** ", " ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*       ", "      *"
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*       ", "*     *"
+        ));
+
+        System.out.println(String.join(" ",
+                " ***** ", " ***** ", "*       ", " ***** "
+        ));
+    }
+}
+
